5cb13bd8e654dcd4d151226280e755682db6a2a3
taolaqa123 <s3810148@rmit.edu.vn>
2024-01-17 18:54:01 +0700

    User Authentication Intergration

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+from datetime import datetime, timedelta, timezone
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, unset_jwt_cookies, jwt_required, JWTManager 
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS 
+from uuid import uuid4
 import pymysql
 
 application = Flask(__name__)
@@ -17,6 +23,16 @@ migrate = Migrate(app, db)
 # Refresh connection after 1 hour
 SQLALCHEMY_POOL_RECYCLE = 3600
 SQLALCHEMY_POOL_TIMEOUT = 20
+
+#Quang Anh
+CORS(app, supports_credentials=True)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+jwt = JWTManager(app)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
+
+bcrypt = Bcrypt(app)
 
 
 # ---- Models ----
@@ -40,21 +56,106 @@ class Payment(db.Model):
     def __repr__(self):
         return f'<User: {self.id}'
 
+def get_uuid():
+    return uuid4().hex
 
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-    email = db.Column(db.String(20))
-    password = db.Column(db.String(80))
-    tier = db.Column(db.String(10))
+    id = db.Column(db.String(11), primary_key=True, unique=True, default=get_uuid)
+    name = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    tier = db.Column(db.String(10), default = 'free')
 
     payment = db.relationship('Payment', backref='user', lazy=True)
     document = db.relationship('Document', backref='user', lazy=True)
 
     def __repr__(self):
         return f'User #{self.id}: {self.name}\n'
+    
+#Quang Anh
+@app.route('/logintoken', methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+  
+    user = User.query.filter_by(email=email).first()
+    
+    if user is None:
+        return jsonify({"error": "Wrong email or passwords"}), 401
+      
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Unauthorized"}), 401
+      
+    access_token = create_access_token(identity=email)
+  
+    return jsonify({
+        "email": email,
+        "access_token": access_token
+    })
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    name = request.json["name"]
+    email = request.json["email"]
+    password = request.json["password"]
+   
+    user_exists = User.query.filter_by(email=email).first() is not None
+   
+    if user_exists:
+        return jsonify({"error": "Email already exists"}), 409
+       
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(name=name, email=email, password=hashed_password, tier='free')
+    db.session.add(new_user)
+    db.session.commit()
+   
+    return jsonify({
+        "id": new_user.id,
+        "email": new_user.email
+    })
+
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token 
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+    
+@app.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
+@app.route('/profile/<getemail>')
+@jwt_required() 
+def my_profile(getemail):
+    print(getemail)
+    if not getemail:
+        return jsonify({"error": "Unauthorized Access"}), 401
+       
+    user = User.query.filter_by(email=getemail).first()
+  
+    response_body = {
+        "id": user.id,
+        "name": user.name,
+        "email": user.email,
+        "tier" : user.tier
+    }
+  
+    return response_body
     
 
 db.create_all()
